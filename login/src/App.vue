baf2ca4aa6ce8b5e560b2b24d9e4b4704e1ce3a7
Rony Al Mawla <ronyalmawla@gmail.com>
2025-10-25 14:57:08 +0300

    Sign up component

--- login/src/App.vue
+++ login/src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <signUp/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/SignUp.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignUp
   }
 }
 </script>
+
 
 <style>
 #app {
---
+++ login/src/components/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+    <img class="logo" src="../assets/signup.png" />
+<h1>Sign Up</h1>
+<div class="register">
+    <input type="text" placeholder="Enter your name"/>
+    <input type="text" placeholder="Enter your email"/>
+    <input type="password" placeholder="Enter your password"/>
+    <button>Sign Up</button>
+</div>
+</template>
+
+<script>
+export default {
+name : 'SignUp'
+}
+
+</script>
+
+<style>
+.logo{
+    width:100px
+}
+.register input{
+    width:300px;
+    height:40px;
+    padding-left:20px;
+    display:block;
+    margin-bottom:30px;
+    margin-right:auto;
+    margin-left:auto;
+    border: 1px solid skyblue;
+}
+.register button{
+    width:320px;
+    height:40px;
+    border:1px solid skyblue;
+    background-color:skyblue;
+    color:#fff;
+    cursor:pointer;
+
+}
+</style>
